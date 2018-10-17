ce735854c25ee9031dde52a0d7384684b89c577d
wanghaoming <whimmy@126.com>
2018-10-17 14:37:05 +0800

    add spring-webflux-intro

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Module | Article
 --|--
 reactive-websocket | [Reactive WebSockets with Spring 5](http://www.baeldung.com/spring-5-reactive-websockets)
+spring-webflux-intro | [Guide to Spring 5 WebFlux](https://www.baeldung.com/spring-webflux)
---
+++ spring-webflux-intro/src/main/java/com/ymmihw/springframework/web/reactive/EmployeeSpringApplication.java
@@ -0,0 +1,17 @@
+package com.ymmihw.springframework.web.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpringApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(EmployeeSpringApplication.class, args);
+
+    EmployeeWebClient employeeWebClient = new EmployeeWebClient();
+    employeeWebClient.consume();
+  }
+
+}
