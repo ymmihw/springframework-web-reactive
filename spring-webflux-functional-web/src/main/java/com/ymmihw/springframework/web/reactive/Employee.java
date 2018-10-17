df6933e1682415fd08e8358fc0e1b7cc1a105ffd
wanghaoming <whimmy@126.com>
2018-10-17 15:30:51 +0800

    add spring-webflux-functional-web

---
+++ spring-webflux-functional-web/src/main/java/com/ymmihw/springframework/web/reactive/Employee.java
@@ -0,0 +1,13 @@
+package com.ymmihw.springframework.web.reactive;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+  private String id;
+  private String name;
+}
---
+++ spring-webflux-functional-web/src/main/java/com/ymmihw/springframework/web/reactive/EmployeeSpringFunctionalApplication.java
@@ -0,0 +1,11 @@
+package com.ymmihw.springframework.web.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpringFunctionalApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EmployeeSpringFunctionalApplication.class, args);
+  }
+}
